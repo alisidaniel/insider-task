b9f5eb979bddb14ed260086d167693d0f6567c29
alisidaniel <alisidaniel@gmail.com>
2022-02-08 22:41:09 +0200

    ui updates

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/league/results', function (){
+    return view('prediction');
+});
